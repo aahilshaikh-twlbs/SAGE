d08780dfbe9598fac383c07379d7af2dbfc8ce07
Aahil Shaikh <aahil.shaikh@twelvelabs.io>
2025-08-14 01:39:51 +0000

    UNST: add in backend ip to env local 3

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -19,10 +19,10 @@ const nextConfig: NextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: '/api/:path*',
-        destination: (process.env.NEXT_PUBLIC_API_URL || 'http://209.38.142.207:8000') + '/:path*',
-      },
+      // {
+      //   source: '/api/:path*',
+      //   destination: (process.env.NEXT_PUBLIC_API_URL || 'http://209.38.142.207:8000') + '/:path*',
+      // },
     ];
   },
 };
